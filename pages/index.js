aa32b7ff209bb11d833624ebca01cf1e87db45e7
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2024-02-05 21:29:13 +0200

    starting routes learn api

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import { useRef, useState } from 'react';
+
+function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  async function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+      const rawResponse = await fetch('/api/feedback', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(reqBody)
+      });
+      const content = await rawResponse.json();
+    
+      console.log(content);
+
+  }
+
+
+
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Feedback</label>
+          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <hr />
+
+    </div>
+  );
+}
+
+export default HomePage;
