8ecd9a93ea8c50fd73df34ad1751a1e17cf14333
Mohamed22221 <hamdymohame7@gmail.com>
2024-02-07 01:21:28 +0200

    add server side props

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
+import { buildFeedbackPath, extractFeedback } from "./api/feedback";
+import { useRouter } from "next/router";
 
-function HomePage() {
-  const [feedbackItems, setFeedbackItems] = useState([]);
-
+function HomePage(props) {
+  // const [feedbackItems, setFeedbackItems] = useState([]);
+  // const [sendItems, setSendItems] = useState([]);
+  const router = useRouter();
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
+
   async function submitFormHandler(event) {
     event.preventDefault();
 
@@ -12,40 +16,59 @@ function HomePage() {
     const enteredFeedback = feedbackInputRef.current.value;
     const reqBody = { email: enteredEmail, text: enteredFeedback };
 
-      const rawResponse = await fetch('/api/feedback', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(reqBody)
-      });
-      const content = await rawResponse.json();
-    
-      console.log(content);
+    const rawResponse = await fetch("/api/feedback", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqBody),
+    });
+    // const content = await rawResponse.json();
+    // setSendItems(content);
 
+    if (rawResponse.status < 300) {
+      router.replace(router.asPath);
+
+    }
   }
 
-
-
+  // useEffect(() => {
+  //   fetch("/api/feedback")
+  //     .then((res) => res.json())
+  //     .then((data) => setFeedbackItems(data.feedback));
+  // }, [sendItems]);
+  // console.log(feedbackItems);
   return (
     <div>
       <h1>The Home Page</h1>
       <form onSubmit={submitFormHandler}>
         <div>
-          <label htmlFor='email'>Your Email Address</label>
-          <input type='email' id='email' ref={emailInputRef} />
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
-          <label htmlFor='feedback'>Your Feedback</label>
-          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
       <hr />
-
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li>{item?.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const filePath = buildFeedbackPath();
+  const feedbackItems = extractFeedback(filePath);
 
+  // Pass data to the page via props
+  return { props: { feedbackItems: feedbackItems } };
+}
 export default HomePage;
+
